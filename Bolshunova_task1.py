7834c080dee8d6efab730dc5e02e097f7b0ec350
Bolshunovaaa <127662235+Bolshunovaaa@users.noreply.github.com>
2023-05-30 16:50:39 +0300

    Delete Bolshunova_task1.py

--- Bolshunova_task1.py
+++
@@ -1,8 +0,0 @@
-# Multiparadigmatic  programming languages:Task_1
-# Bolshunova Kateryna:IKM-221a
-
-print('Multiparadigmatic  programming languages:Task_1\nBolshunova Kateryna:IKM-221a')
-
-print(('Vladislav, ' * 44)[:-2])
-
-print(((15 - (13.1 / 3)) / (33**2 + 19.3)) + 3.3)
